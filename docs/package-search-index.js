043a801c7c93d5bf3554c7b7c9423a9c8f85673b
Miguel Angel <146874956+Miguel18201@users.noreply.github.com>
2023-10-26 10:18:10 -0600

    Adición de javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"p3"},{"l":"poop3"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"p3","l":"Agenda"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"p3","l":"Diccionario"},{"p":"poop3","l":"POOP3"}];updateSearchResults();
